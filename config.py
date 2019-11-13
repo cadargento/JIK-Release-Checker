8980c1e478a76d6a924b6df0447e827b68b0acb3
G Ferrara <52937317+giucecrew@users.noreply.github.com>
2019-11-13 09:11:02 -0500

    Update config.py

--- config.py
+++ config.py
@@ -1,7 +1,7 @@
 # from config import CLIENT_ID, CLIENT_SECRET, PLAY_LIST, USER
 
-CLIENT_ID = 'dfe4f3932d3843b6be02906e9a024408'
-CLIENT_SECRET = '5cdb0f28717b4be687626a1621829be6'
-MY_ADDRESS = 'jikautomated@gmail.com'
-PASSWORD = 'Jesus1sK1ng'
+CLIENT_ID = ''
+CLIENT_SECRET = ''
+MY_ADDRESS = ''
+PASSWORD = ''
 
